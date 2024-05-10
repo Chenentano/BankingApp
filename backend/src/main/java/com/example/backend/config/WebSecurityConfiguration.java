f9585925aecc910abd6f69b31261962f5a660c88
basti <bastian_behrendt@yahoo.de>
2024-05-10 16:17:42 +0100

    Refactor CRSF disable method in web security configuration

--- backend/src/main/java/com/example/backend/config/WebSecurityConfiguration.java
+++ backend/src/main/java/com/example/backend/config/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,9 +21,7 @@ public class WebSecurityConfiguration {
         })
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
-                .csrf(httpSecurityCorsConfigurer -> {
-                    httpSecurityCorsConfigurer.disable();
-                })
+                .csrf(AbstractHttpConfigurer::disable)
         .build();
     }
 }
