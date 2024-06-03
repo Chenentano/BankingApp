8bb4d02df9be762166cca8a812fec9247ce13f20
basti <bastian_behrendt@yahoo.de>
2024-06-03 16:44:34 +0100

    Implemented Chatbot with ML

--- frontend/src/components/chatbot/chatbot.jsx
+++ frontend/src/components/chatbot/chatbot.jsx
@@ -6,8 +6,12 @@ class Chatbot extends Component {
         if (!window.kommunicate) {
             (function(d, m){
                 var kommunicateSettings = {"appId":"<db447252d5a393cef5d729181537b4a7>","popupWidget":true,"automaticChatOpenOnNavigation":true};
-                var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                s.async = true;
                 s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+                s.integrity = "sha384-expectedHashHere";
+                s.crossOrigin = "anonymous";
                 const h = document.getElementsByTagName("head")[0];
                 h.appendChild(s);
                 window.kommunicate = m; m._globals = kommunicateSettings;
